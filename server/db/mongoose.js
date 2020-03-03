1f937b8c709829f0674dd28f02bd8836e007da9d
Arnav <arnavpanwar99@gmail.com>
2020-03-03 13:33:37 +0530

    hello

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,7 +9,8 @@ const URL = 'mongodb+srv://arnav:arnavozil@cluster0-laaif.mongodb.net/test?retry
 
 const URI = 'mongodb://localhost:27017/TodoApp';
 
-mongoose.connect(URL, configObject).then(() => console.log('connected'))
+mongoose.connect(URL || URI, configObject).then(() => console.log('connected'))
+.catch((err) => console.log('there is some error'));
 
 module.exports = {
     mongoose,
